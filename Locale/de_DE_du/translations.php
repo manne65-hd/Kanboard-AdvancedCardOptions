5372253930e3e87bb8a6719c98daadfa4e57d757
manne65-hd <manne@hoffmaenner.net>
2021-04-18 07:27:35 +0200

    Added more translation

--- Locale/de_DE_du/translations.php
+++ Locale/de_DE_du/translations.php
@@ -1,11 +1,22 @@
 <?php
 
 return array(
-    'Allows advanced control over the appearance and options of the task-cards in board, list and dashboard-view' => 'Ermöglicht erweiterte Kontrolle über das Erscheinungsbild und die Optionen der Aufgaben-Karten in Pinnwand, Listen und Dashboard-Ansicht',
+    'Allows advanced control over the appearance and options of the task-cards in board-view' => 'Ermöglicht erweiterte Kontrolle über das Erscheinungsbild und die Optionen der Aufgaben-Karten in der Pinnwand-Ansicht',
+    'Advanced card-options' => 'Erweiterte Karten-Optionen',
     'Advanced card-options for the project' => 'Erweiterte Karten-Optionen für das Projekt',
+    'Configuration-method' => 'Konfigurations-Methode',
+    'Use the applications-defaults (configured in > ADMIN / Settings / Advanced card-options)' => 'Nutze die Anwendungs-Vorgaben (zu konfigurieren in > ADMIN / Einstellungen / Erweiterte Karten-Optionen)',
+    'Use custom values for this project' => 'Individuelle Werte für dieses Projekt nutzen',
+    'All settings listed below will be IGNORED, when using the application-defaults!' => 'Alle unten aufgelisteten Einstellungen werden IGNORIERT, wenn die Anwendungs-Vorgaben genutzt werden!',
+    'Buttons to change the due-date' => 'Schaltflächen zum Verändern des Fälligkeitsdatums',
+    'Number of days to push the due-date:' => 'Anzahl an Tagen, um die das Fälligkeitsdatum verschoben werden soll',
+    'Leave blank or set to 0 to disable button' => 'Leer lassen oder auf 0 setzen, um Schaltfäche zu deaktivieren',
+    '1st Button' => '1. Schaltfäche',
+    '2nd Button' => '2. Schaltfäche',
+    '3rd Button' => '3. Schaltfäche',
     'Task due-date has been pushed successfully.' => 'Das Fälligkeitsdatum der Aufgabe wurde erfolgreich verschoben',
-    'Error while trying to push the due-date of this task.' => 'Fehler beim Versuch das Fälligkeitsdatum der Aufgabe zu verschieben.',
+    'Error while trying to push teh due-date of this task.' => 'Fehler beim Versuch das Fälligkeitsdatum der Aufgabe zu verschieben.',
     'Push the task\'s due-date' => 'Fälligkeitsdatum der Aufgabe verschieben',
-    'Do you really want to push the due date of the task "%s" %s days out?' => 'Willst du die Fälligkeit der Aufgabe "%s" wirklich um %s Tage verschieben?',
+    'Do you really want to push out the due date of the task "%s" for %s day(s)?' => 'Willst du die Fälligkeit der Aufgabe "%s" wirklich um %s Tag(e) verschieben?',
     'Day' => 'Tag',
 );
